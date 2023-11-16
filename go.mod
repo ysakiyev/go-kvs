c6dbf952a48bda8ddc328375b5478a63ff79b04a
Yermek Sakiyev <ysakiyev@gmail.com>
2023-11-16 06:12:32 -0700

    added zerolog

--- go.mod
+++ go.mod
@@ -11,8 +11,11 @@ require (
 	github.com/chzyer/readline v0.0.0-20180603132655-2972be24d48e // indirect
 	github.com/golang/protobuf v1.5.3 // indirect
 	github.com/manifoldco/promptui v0.9.0 // indirect
+	github.com/mattn/go-colorable v0.1.13 // indirect
+	github.com/mattn/go-isatty v0.0.19 // indirect
+	github.com/rs/zerolog v1.31.0 // indirect
 	golang.org/x/net v0.14.0 // indirect
-	golang.org/x/sys v0.11.0 // indirect
+	golang.org/x/sys v0.12.0 // indirect
 	golang.org/x/text v0.12.0 // indirect
 	google.golang.org/genproto/googleapis/rpc v0.0.0-20230822172742-b8732ec3820d // indirect
 )
--- server/cmd/main.go
+++ server/cmd/main.go
@@ -1,24 +1,27 @@
 package main
 
 import (
+	"github.com/rs/zerolog"
+	"github.com/rs/zerolog/log"
 	pb "go-kvs/proto/pb"
+	g "go-kvs/server/grpc"
 	"go-kvs/server/kvs"
 	"google.golang.org/grpc"
-	"log"
 	"net"
 )
 
-import g "go-kvs/server/grpc"
-
 func main() {
+	zerolog.SetGlobalLevel(zerolog.InfoLevel)
+
 	lis, err := net.Listen("tcp", ":50051")
 	if err != nil {
-		log.Fatalf("Failed to listen: %v", err)
+		log.Fatal().Msgf("Failed to listen: %v", err)
 	}
 	grpcServer := grpc.NewServer()
 	pb.RegisterGoKvsServer(grpcServer, g.NewKvsServer(kvs.New()))
 
+	log.Info().Msg("Listening on :50051")
 	if err := grpcServer.Serve(lis); err != nil {
-		log.Fatalf("Failed to serve: %v", err)
+		log.Fatal().Msgf("Failed to serve: %v", err)
 	}
 }
